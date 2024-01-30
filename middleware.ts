9babdb273a07f96bb5b1d5f103c221bf1438dd01
thegypsyboyo <lewisonyango9@gmail.com>
2024-01-30 09:51:06 +0300

    Added webhook secret

--- middleware.ts
+++ middleware.ts
@@ -6,8 +6,15 @@ import { authMiddleware } from '@clerk/nextjs';
 export default authMiddleware({
   publicRoutes: [
     '/',
+    '/api/webhook',
+    '/question/:id',
+    '/tags',
+    '/tags/:id',
+    '/profile/:id',
+    '/community',
+    '/jobs'
   ],
-  ignoredRoutes: ['/api/webhook']
+  ignoredRoutes: ['/api/webhook', '/api/chatgpt']
 });
 
 export const config = {
